d486b5d9edbe042a6710cafcba988e7c0faeecaf
bmurodullayev1114 <bmurodullayev1114@gmail.com>
2022-03-12 12:41:12 +0500

    'dddddd'

--- data/config.py
+++ data/config.py
@@ -6,19 +6,8 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+CHANNELS = env.list("CHANNELS")
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
-
-CHANNELS = ["@komilon_savdo"]
-
-# import os
-#
-# # env fayl ichidan quydagilarni o'qiymiz
-# BOT_TOKEN=str(os.environ.get("BOT_TOKEN")) #Bot token
-# ADMINS=str(os.environ.get("ADMINS")) #Admin ro'yxati
-# IP=str(os.environ.get("ip")) #Xosting ip manzili
-# PROVIDER_TOKEN=str(os.environ.get("PROVIDER_TOKEN"))
-# CHANNELS=str(os.environ.get("CHANNELS"))
-
 
 
